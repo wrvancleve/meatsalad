b90be1c0adc177a175fa0bd3f6f0743d5c0e848d
William Van Cleve <wvancleve3@gmail.com>
2024-06-30 18:44:42 -0500

    Many changes

--- kubejs/server_scripts/level_events.js
+++ kubejs/server_scripts/level_events.js
@@ -1,4 +1,5 @@
 LevelEvents.loaded(event => {
+  Utils.server.runCommandSilent('ftbteams server create Meat')
   Utils.server.runCommandSilent('team add Meat')
   Utils.server.runCommandSilent('team modify Meat friendlyFire false')
 })
