505c8d0b88ec08346192e10764191ff230dad3ac
William Van Cleve <wvancleve3@gmail.com>
2024-12-13 12:26:52 -0600

    Update run forge version

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-screen -S meatsalad java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.32/unix_args.txt nogui "$@"
+screen -S meatsalad java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.11/unix_args.txt nogui "$@"
